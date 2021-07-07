53fd150538e055bdb14c49ceb0d2f41463742922
d3ntlyname <84956808+d3ntlyname@users.noreply.github.com>
2021-07-07 15:00:15 +0300

    Update bot1.py

--- bot1.py
+++ bot1.py
@@ -13,12 +13,4 @@ def spam(self, msg):
   msg.reply("Кaжется, рeйд нaчинается...\nby @dently and @vellogs")
   sleep(1)
 
-bot2 = Client("bot2", '5235407', '07423ead8b4192e7650b14bb532fa037')
-
-@bot2.on_message(filters.command("raid", prefixes="_"))
-def spam(self, msg):
- for _ in range(100):
-  msg.reply("Кaжется, рeйд нaчинается...\nby @dently and @v3llogs")
-  sleep(1)
-
 bot1.run()
