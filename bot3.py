b62f7984ac869aa953cd18239a59301939354be6
d3ntlyname <84956808+d3ntlyname@users.noreply.github.com>
2021-07-07 14:23:17 +0300

    Add files via upload

---
+++ bot3.py
@@ -0,0 +1,16 @@
+from pyrogram import Client, filters
+from pyrogram.types import InputMediaPhoto
+from threading import Timer
+from time import sleep
+import random
+
+
+bot3 = Client("bot3", '6531400', 'c1b044b3cb11b14d79b4e0e2c11b5cc1')
+
+@bot3.on_message(filters.command("raid", prefixes="_"))
+def spam(self, msg):
+ for _ in range(100):
+  msg.reply("Кaжется, рeйд нaчинается...\nby @dently")
+  sleep(1)
+
+bot3.run()
