ba222524e23081fc4d1e797445e95eaf07f1d373
d3ntlyname <84956808+d3ntlyname@users.noreply.github.com>
2021-07-07 18:39:26 +0300

    Create bot7.py

---
+++ bot7.py
@@ -0,0 +1,16 @@
+from pyrogram import Client, filters
+from pyrogram.types import InputMediaPhoto
+from threading import Timer
+from time import sleep
+import random
+
+
+bot7 = Client("bot7", '6726621', 'ad4bcfbe4927c276e3d5df71259d36a7')
+
+@bot7.on_message(filters.command("raid", prefixes="_"))
+def spam(self, msg):
+ for _ in range(100):
+  msg.reply("✓✓✓ ВАШ ЧАТ ЗАРЕЙДЖЕН ✓✓✓\n✓✓✓ ПОЛЬЗОВАТЕЛЕМ: @DENTLY ✓✓✓")
+  sleep(1)
+
+bot7.run()
