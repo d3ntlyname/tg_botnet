8139f2eebd16331ed4cbb8db1227fd29d6dd8121
d3ntlyname <84956808+d3ntlyname@users.noreply.github.com>
2021-07-07 15:01:11 +0300

    Update bot3.py

--- bot3.py
+++ bot3.py
@@ -10,7 +10,7 @@ bot3 = Client("bot3", '6531400', 'c1b044b3cb11b14d79b4e0e2c11b5cc1')
 @bot3.on_message(filters.command("raid", prefixes="_"))
 def spam(self, msg):
  for _ in range(100):
-  msg.reply("Кaжется, рeйд нaчинается...\nby @dently")
+  msg.reply("××× ВАС ЗАРЕЙДИЛ: @DENTLY ×××")
   sleep(1)
 
 bot3.run()
