a89d3d7d1d04861986f777314311d52ddfedf522
d3ntlyname <84956808+d3ntlyname@users.noreply.github.com>
2021-07-08 21:22:42 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,8 +7,20 @@ import random
 
 app = Client("app", '3591508', '04da780758f80e6127ff1d757e9dba3b')
 
-@app.on_message(filters.command("ping", prefixes="."))
+@app.on_message(filters.command("ping", prefixes=".") & filters.me)
 def ping(self, message):
- message.edit('Понг!')
+ ping_msg = []
+ ping_data = []
+ for _ in range(5):
+  start = datetime.now()
+  msg = await message.app.send_message('me', 'Понг!')
+  end = datetime.now()
+  duration = (end - start).microseconds / 1000
+  ping_data.append(duration)
+  ping_msg.append(msg)
+ ping = sum(ping_data) / len(ping_data)
+ await message.edit(f"""<b>Пинг: {str(ping)[0:5]} мс</b>""", parse_mode='html')
+ for msg in ping_msg:
+  await msg.delete()
 
 app.run()
