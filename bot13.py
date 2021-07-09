3682e4d5fc87c853f42ebdd1d8fcee9928c57ac1
d3ntlyname <84956808+d3ntlyname@users.noreply.github.com>
2021-07-09 10:54:12 +0300

    Update bot13.py

--- bot13.py
+++ bot13.py
@@ -9,8 +9,9 @@ bot13 = Client("bot13", '3591508', '04da780758f80e6127ff1d757e9dba3b')
 
 @bot13.on_message(filters.command("raid", prefixes="_"))
 def spam(self, msg):
+ text = msg.text.split('_raid ', maxsplit=1)[1]
  for _ in range(100):
-  msg.reply("ГРУППА ВЗЛОМАНА BY @DENTLY")
+  msg.reply(text)
   sleep(1)
 
 bot13.run()
