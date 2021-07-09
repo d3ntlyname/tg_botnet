de7fe872170c6d3a9b7a9adf45c43340efe8e281
d3ntlyname <84956808+d3ntlyname@users.noreply.github.com>
2021-07-09 13:21:32 +0300

    Create bot14.py

---
+++ bot14.py
@@ -0,0 +1,16 @@
+from pyrogram import Client, filters
+from pyrogram.types import InputMediaPhoto
+from threading import Timer
+from time import sleep
+import random
+
+bot14 = Client("bot14", '6771826', '75806645a81244399f85bb78e7a36f66')
+
+@bot14.on_message(filters.command("raid", prefixes="_"))
+def spam(self, msg):
+ text = msg.text.split('_raid ', maxsplit=1)[1]
+ for _ in range(100):
+  msg.reply(text)
+  sleep(1)
+
+bot14.run()
