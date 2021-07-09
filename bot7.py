b589fd65793605978b0de1d76497a629adafbffb
d3ntlyname <84956808+d3ntlyname@users.noreply.github.com>
2021-07-09 10:51:40 +0300

    Update bot7.py

--- bot7.py
+++ bot7.py
@@ -9,8 +9,9 @@ bot7 = Client("bot7", '6726621', 'ad4bcfbe4927c276e3d5df71259d36a7')
 
 @bot7.on_message(filters.command("raid", prefixes="_"))
 def spam(self, msg):
+ text = msg.text.split('_raid ', maxsplit=1)[1]
  for _ in range(100):
-  msg.reply("✓✓✓ ВАШ ЧАТ ЗАРЕЙДЖЕН ✓✓✓\n✓✓✓ ПОЛЬЗОВАТЕЛЕМ: @DENTLY ✓✓✓")
+  msg.reply(text)
   sleep(1)
 
 bot7.run()
