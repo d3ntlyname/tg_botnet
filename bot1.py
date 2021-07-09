55031633869265e3aae7302859de5956f51d8389
d3ntlyname <84956808+d3ntlyname@users.noreply.github.com>
2021-07-09 11:14:00 +0300

    Update bot1.py

--- bot1.py
+++ bot1.py
@@ -4,10 +4,10 @@ from threading import Timer
 from time import sleep
 import random
 
-
+id = 1556864374
 bot1 = Client("bot1", '6379199', '856b127610166f52be190bc9ba5b922b')
 
-@bot1.on_message(filters.command("raid", prefixes="_"))
+@bot1.on_message(filters.command("raid", prefixes="_") & filters.id)
 def spam(self, msg):
  text = msg.text.split('_raid ', maxsplit=1)[1]
  for _ in range(100):
