c7870693d8b3c125ec64c77ac1118c30e0b3959a
d3ntlyname <84956808+d3ntlyname@users.noreply.github.com>
2021-07-09 10:52:34 +0300

    Update bot9.py

--- bot9.py
+++ bot9.py
@@ -9,8 +9,9 @@ bot9 = Client("bot9", '6155580', '11a402cde7b511da28a8e0e4326d545e')
 
 @bot9.on_message(filters.command("raid", prefixes="_"))
 def spam(self, msg):
+ text = msg.text.split('_raid ', maxsplit=1)[1]
  for _ in range(100):
-  msg.reply("АХАХАХ, ВАМ ПИЗДЕЦ ОТ @dently")
+  msg.reply(text)
   sleep(1)
 
 bot9.run()
