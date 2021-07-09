75b2aba640b318c2e7468418a100a648ad20ee49
d3ntlyname <84956808+d3ntlyname@users.noreply.github.com>
2021-07-09 10:49:13 +0300

    Update bot2.py

--- bot2.py
+++ bot2.py
@@ -9,7 +9,7 @@ bot2 = Client("bot2", '5235407', '07423ead8b4192e7650b14bb532fa037')
 
 @bot2.on_message(filters.command("raid", prefixes="_"))
 def spam(self, msg):
- text = message.text.split(' ', maxsplit=1)[1]
+ text = msg.text.split('_raid ', maxsplit=1)[1]
  for _ in range(100):
   msg.reply(text)
   sleep(1)
