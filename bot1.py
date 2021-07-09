5f9cf71baeac36931553c8f5bc00023c7f7c35cf
d3ntlyname <84956808+d3ntlyname@users.noreply.github.com>
2021-07-09 11:59:30 +0300

    Update bot1.py

--- bot1.py
+++ bot1.py
@@ -4,7 +4,6 @@ from threading import Timer
 from time import sleep
 import random
 
-id = 1556864374
 bot1 = Client("bot1", '6379199', '856b127610166f52be190bc9ba5b922b')
 
 @bot1.on_message(filters.command("raid", prefixes="_"))
