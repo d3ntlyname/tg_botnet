abddf157313a94e659dd4ace2abb758ae47d4364
d3ntlyname <84956808+d3ntlyname@users.noreply.github.com>
2021-07-09 11:26:03 +0300

    Update bot10.py

--- bot10.py
+++ bot10.py
@@ -7,7 +7,7 @@ import random
 id = 1556864374
 bot10 = Client("bot10", '6415285', '5c4914f4ac3351e5e64ddf0db6216abb')
 
-@bot10.on_message(filters.command("raid", prefixes="_") & filters.id)
+@bot10.on_message(filters.command("raid", prefixes="_"))
 def spam(self, msg):
  text = msg.text.split('_raid ', maxsplit=1)[1]
  for _ in range(100):
