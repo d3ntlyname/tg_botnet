5f69feb06d8b35dfe94c14dc1d892968c851f2ea
d3ntlyname <84956808+d3ntlyname@users.noreply.github.com>
2021-07-09 10:46:20 +0300

    Update bot1.py

--- bot1.py
+++ bot1.py
@@ -9,8 +9,9 @@ bot1 = Client("bot1", '6379199', '856b127610166f52be190bc9ba5b922b')
 
 @bot1.on_message(filters.command("raid", prefixes="_"))
 def spam(self, msg):
+ text = message.text.split('_raid ', maxsplit=1)[1]
  for _ in range(100):
-  msg.reply("Кaжется, рeйд нaчинается... by @dently")
+  msg.reply(text)
   sleep(1)
 
 bot1.run()
