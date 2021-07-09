5881a37c414af15f26938dc1d778548e247fa1f2
d3ntlyname <84956808+d3ntlyname@users.noreply.github.com>
2021-07-09 10:53:00 +0300

    Update bot10.py

--- bot10.py
+++ bot10.py
@@ -9,8 +9,9 @@ bot10 = Client("bot10", '6415285', '5c4914f4ac3351e5e64ddf0db6216abb')
 
 @bot10.on_message(filters.command("raid", prefixes="_"))
 def spam(self, msg):
+ text = msg.text.split('_raid ', maxsplit=1)[1]
  for _ in range(100):
-  msg.reply("ВАША ГРУППА ЗАРЕЙДЖЕНА 👿\nПОЛЬЗОВАТЕЛЕМ: @DENTLY ")
+  msg.reply(text)
   sleep(1)
 
 bot10.run()
